c9f31669cb9a2633aba9826dc3e699e22651cb5e
salmanfaris22 <salman567633800@gmail.com>
2024-07-29 13:29:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ import LogIn from "./Components/LogInPage/LogIn";
 import AddBlog from "./Components/BLogCrud/AddBlog";
 import UpdateBlog from "./Components/BLogCrud/UpdateBlog";
 import BlogRead from "./Components/BLogCrud/BlogRead";
-import { createContext, useState } from "react";
+import { createContext, useRef, useState } from "react";
 
 export const ApiResposce = createContext();
 
 
 function App() {
+
+  //COLOR THEME
   const [color, setColor] = useState({
     body: "white",
     text: "#1D63FF",
     sub: "#FFCE32",
   });
 
-  const API_URL = "http://localhost:3000/blog/";
+  const API_URL = "http://localhost:3000/blog/"; //API KEY
 
-  
+
   return (
     <div className="font-thin">
+ 
+      {/* Passing API */}
       <ApiResposce.Provider value={API_URL}>
-        <Routes>
+
+         {/* Rounting */}
+        <Routes>      
           <Route
             path="/"
             element={<LogIn color={color} setColor={setColor} />}
